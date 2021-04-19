bbc4c918b1aced3bd7a24377ecdc5362e9476c5f
samuelowino <samuelowino43@yahoo.com>
2021-04-19 19:54:04 +0300

    Section 4: General integer and double input

---
+++ ch2/section4/sample1.cpp
@@ -0,0 +1,17 @@
+//Input Receives Interger and Double precision input
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    int i;
+    cout << "Enter that number dowg... \n"; 
+    cin >> i;
+
+    double d;
+    cout << "Enter that double precis' number hommy... \n";
+    cin >> d;
+
+    cout << "These are the deeds, number " << i << " double precision input " << d << "\n";
+}
