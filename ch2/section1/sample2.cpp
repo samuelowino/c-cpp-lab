51ab868a1985bb4f50dfd85c64b6f8d4b2d87e0d
samuelowino <samuelowino43@yahoo.com>
2021-04-19 18:09:39 +0300

    Section 1: Additions Standard Library Headers with std::

---
+++ ch2/section1/sample2.cpp
@@ -0,0 +1,10 @@
+//Other examples of standard library headers
+#include <iostream>
+#include <string>
+#include <list>
+
+int main(){
+    std::string carsonQuote = "Those who dwell among the beauties 
+                                and mysteries of the earth are never alone or weary of life";
+    std::list<std::string> fancySlogans;
+}
