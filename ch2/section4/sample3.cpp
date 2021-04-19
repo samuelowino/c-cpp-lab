b94918835946f8f905afe0475f451aed344dfbd0
samuelowino <samuelowino43@yahoo.com>
2021-04-19 20:18:27 +0300

    Section 4: Text Input, Withou support for spaces

---
+++ ch2/section4/sample3.cpp
@@ -0,0 +1,17 @@
+//Input, Text Input: Without Spaces
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string str;
+
+    cout << "Which country in the world is covered mostly in salt" << endl;
+    cin >> str;
+    cout << "Incorrect answer " << str << "!\n The Correct answer is " << str << " of napolia";
+
+    cout << "Catch 22: You cannot type in the phrase \" of napolia \" \n 
+                    Refer to sample4 of section 4";
+    
+}
