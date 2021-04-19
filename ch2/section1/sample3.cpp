013b6d03531a06ce7425369dfabeff67495520eb
samuelowino <samuelowino43@yahoo.com>
2021-04-19 18:20:26 +0300

    Section 1: Make std:: names available minus prefix

---
+++ ch2/section1/sample3.cpp
@@ -0,0 +1,10 @@
+//Omit std:: namespace declarations
+#include <iostream>
+#include <string>
+
+using namespace std; //Make standard namespace available without std:: perfixies
+
+int main(){
+    string slogan = "He that planteth a tree is a servant of God...";
+    cout<<"Company Slogan of the month " + slogan + " !"<<endl;
+}
