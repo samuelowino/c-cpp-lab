1e80c9889df14f9232b676f50b4e056468876f75
samuelowino <samuelowino43@yahoo.com>
2021-04-21 02:54:07 +0300

    Section 9: Algorithms: Standard Library Algorithms list

---
+++ ch2/section9/sample6.cpp
@@ -0,0 +1,23 @@
+//Section 9: Algorithms: Standard Library Algorithms list
+#include <algorithm> 
+/**
+ * The algorithms are defined in the namespace std::
+ * and presented in the #<algorithm> header.
+ *
+ **/
+
+ /**
+  * A few most useful algos in the std:: namespace
+  **/
+for_each();             //Invoke function for each element
+find();                 //Find first occurence of arguments
+find_if();              //Find first match of predicate
+count();                //Count occurence of element
+count_if();             //Count matches of predicate
+replace();              //Replace element of predicate
+replace_if();           //Replace element that matches predicate with new value
+copy();                 //Copy elements
+unique_copy();          //Copy elements that are not duplicates
+sort();                 //Sort elements
+equal_range();          //Find all elements with equivalent values
+merge();                //Merge sorted sequences 
