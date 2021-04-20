89c44fdc85fe53171940e2042458d4cf8e3502f1
samuelowino <samuelowino43@yahoo.com>
2021-04-20 18:40:27 +0300

    Section 8: A complete list of standard containers

---
+++ ch2/section8/sample1.cpp
@@ -0,0 +1,11 @@
+//List of Standard Containers
+#include <vector>           // A variable-sized vector #vector<T>
+#include <list>             // A doubly-linked list #list<T>
+#include <queue>            // A queue #queue<T>
+#include <stack>            // A stack #stack<T>
+#include <deque>            // A double-ended queue #dequeu<T>
+#include <priority_queueu>  // A queue sorted by value #priority_queue<T>
+#include <set>              // A set #set<T>
+#include <multiset>         // A set in which a value can occur many times #multiset<T>
+#include <map>              // An associative array of key-value pairs #map<key,value>
+#include <multimap>         // A map in which a key can occur many times
