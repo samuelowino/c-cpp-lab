1219f66a976729f5e7c32b468087df34790cb944
samuelowino <samuelowino43@yahoo.com>
2021-04-25 22:23:13 +0300

    Section 3: Arguments; Unspecified Arguments (...)

---
+++ ch7/section3/sample2.cpp
@@ -0,0 +1,17 @@
+//Section 3: Arguments; Unspecified Arguments (...)
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    /**
+     * For some functions, it is not possible to specify
+     * the number and type of all arguments expected in a call.
+     * Such a function is declare by terminating the list 
+     * of argument declarations with the ellipsis (...),
+     * which means
+     *      "and maybe some more arguments."
+     *
+     **/
+    int printf(const char*);
+}
